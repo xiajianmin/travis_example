9f681c9d27f1f72152645b54d7b78cf2e4f85ae6
Alexander Daniel Hadiwijaya <ahadiwijaya@quicketsolutions.com>
2015-09-11 15:23:00 -0500

    first commit

---
+++ models/db.py
@@ -0,0 +1,29 @@
+# original construction for db
+yaodb = DAL('mysql://root:@127.0.0.1/testdb', lazy_tables=False, migrate=False)
+#DAL.define_table
+import datetime
+import time
+
+from gluon.custom_import import track_changes
+track_changes(True)
+
+from gluon.tools import *
+auth = Auth(yaodb)
+# from custom_validators import PASSWORD
+# from helper import check_password_expires, delete_extra_user_sessions
+# from user_helper import get_user_fullname
+# from collections import OrderedDict
+
+## if user's timezone is set, alter request.now to be the user's local time
+is_timezone_unknown = (session.user_timezone is None)
+user_timezone = session.user_timezone or 'UTC'
+if not is_timezone_unknown:
+  from pytz import utc, timezone
+  request.now = utc.localize(request.utcnow, is_dst=None).astimezone(timezone(session.user_timezone)).replace(tzinfo=None)
+
+## configure email
+from gluon.tools import Mail
+mail = Mail()
+# mail = auth.settings.mailer
+mail.settings.server = 'smtp.gmail.com:587'
+auth.settings.mailer = mail
